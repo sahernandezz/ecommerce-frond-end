70d38de37685fda0d5356f75ff7abdd7ac03a84c
Sergio Alejandro Hz <73502611+sahernandezz@users.noreply.github.com>
2025-06-30 18:07:00 -0500

    Suppress hydration mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
         <body
+            suppressHydrationWarning
             className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <ThemeProvider attribute="class">
             <AuthProvider>
